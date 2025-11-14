2636e5e021f1583307c7e8e1718746738bbc6450
DylanC <cabreradylan055@gmail.com>
2025-11-14 16:18:59 -0300

    Superheroes renderizados en el home y boton de recargar funcionando

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,47 +1,31 @@
+import { useEffect, useState } from "react";
+
 export const HomePage = () => {
   // TODO: Integrar lógica para obtener superhéroes desde la API
   // TODO: Implementar useState para almacenar la lista de superhéroes
   // TODO: Implementar función para recargar superhéroes
+  const [superheroes, setSuperheroes] = useState([])
 
-  // Datos de ejemplo para las cards
-  const superheroes = [
-    {
-      id: 1,
-      superhero: "Superman",
-      image:
-        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/644-superman.jpg",
-    },
-    {
-      id: 2,
-      superhero: "Batman",
-      image:
-        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/70-batman.jpg",
-    },
-    {
-      id: 3,
-      superhero: "Wonder Woman",
-      image:
-        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/720-wonder-woman.jpg",
-    },
-    {
-      id: 4,
-      superhero: "Spider-Man",
-      image:
-        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/620-spider-man.jpg",
-    },
-    {
-      id: 5,
-      superhero: "Iron Man",
-      image:
-        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/346-iron-man.jpg",
-    },
-    {
-      id: 6,
-      superhero: "Captain America",
-      image:
-        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/149-captain-america.jpg",
-    },
-  ];
+  const getHeroes = async() => {
+      try {
+        const response = await fetch("http://localhost:3000/api/superheroes", {
+          method: "GET",
+          credentials: "include",
+        });
+        const data = await response.json()
+        console.log(data)
+
+        if (response.ok) {
+          setSuperheroes(data.data);
+        } 
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  
+    useEffect(()=>{
+      getHeroes()
+    }, [])
 
   return (
     <div className="container mx-auto px-4 pb-8">
@@ -53,6 +37,7 @@ export const HomePage = () => {
         <button
           onClick={() => {
             // TODO: Implementar función para recargar superhéroes
+            getHeroes()
           }}
           className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-2 rounded transition-colors"
         >
